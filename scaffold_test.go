372cc52001dc9c48235b8ccd7449462513776b2b
Songmu <y.songmu@gmail.com>
2019-03-03 13:55:53 +0900

    add test

--- scaffold_test.go
+++ scaffold_test.go
@@ -1,11 +1,29 @@
 package gokoku
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
+	"reflect"
 	"testing"
+
+	"golang.org/x/xerrors"
 )
 
+func tempd(t *testing.T) string {
+	tempd, err := ioutil.TempDir("", "gokokutest-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	return tempd
+}
+
 func TestScaffold(t *testing.T) {
+	tdir := tempd(t)
+	defer os.RemoveAll(tdir)
+
 	data := struct {
 		Author, PackagePath        string
 		GitHubHost, Owner, Package string
@@ -19,8 +37,74 @@ func TestScaffold(t *testing.T) {
 		Year:        2019,
 	}
 
-	err := Scaffold(http.Dir("testdata/basic"), ".", ".tmp/scaf22", data)
+	err := Scaffold(http.Dir("testdata/basic"), ".", tdir, data)
 	if err != nil {
 		t.Errorf("something went wrong: %s", err)
 	}
+
+	err = dirDiff(tdir, "./testdata/basic-expect")
+	if err != nil {
+		t.Errorf("something went wrong: %s", err)
+	}
+
+}
+
+func dirDiff(dirA, dirB string) error {
+	errStr := ""
+	err := filepath.Walk(dirA, func(path string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if fi.IsDir() {
+			return nil
+		}
+		rel, err := filepath.Rel(dirA, path)
+		if err != nil {
+			return err
+		}
+		target := filepath.Join(dirB, rel)
+		if fi, err := os.Stat(target); err != nil || fi.IsDir() {
+			errStr += fmt.Sprintf("file %q is missing in %q\n", rel, dirB)
+			return nil
+		}
+		got, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		expect, err := ioutil.ReadFile(target)
+		if err != nil {
+			return err
+		}
+		if !reflect.DeepEqual(expect, got) {
+			errStr += fmt.Sprintf("%q and %q have differ\n", path, target)
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	err = filepath.Walk(dirB, func(path string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if fi.IsDir() {
+			return nil
+		}
+		rel, err := filepath.Rel(dirB, path)
+		if err != nil {
+			return err
+		}
+		target := filepath.Join(dirB, rel)
+		if fi, err := os.Stat(target); err != nil || fi.IsDir() {
+			errStr += fmt.Sprintf("file %q is missing in %q\n", rel, dirA)
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	if errStr != "" {
+		return xerrors.New(errStr)
+	}
+	return nil
 }
